9de723966a82ee02b941bb73d4c8c8c3803622ae
Shaik Dadapeer <shaikdadapeer362@gmail.com>
2021-07-07 14:23:40 +0000

    commit message

---
+++ src/components/ThumbnailItem/index.js
@@ -0,0 +1,30 @@
+// Write your code here.
+import './index.css'
+import {Component} from 'react'
+
+class ThumbnailItem extends Component {
+  onShowImage = () => {
+    const {imageItem, onShowThumbnail, isActive} = this.props
+    const {id} = imageItem
+    onShowThumbnail(id)
+  }
+
+  render() {
+    const {imageItem, isActive} = this.props
+    const {thumbnailUrl, thumbnailAltText} = imageItem
+    const thumbnailClassName = isActive ? `thumbnail active` : `thumbnail`
+    return (
+      <li className="list-style">
+        <button className="btn-style" onClick={this.onShowImage}>
+          <img
+            className={thumbnailClassName}
+            src={thumbnailUrl}
+            alt={thumbnailAltText}
+          />
+        </button>
+      </li>
+    )
+  }
+}
+
+export default ThumbnailItem
